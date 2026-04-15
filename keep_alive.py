08a4a80dbfe02762c205f3be15f82687616efb09
Elisif-f <kuzmina.d.a.95@gmail.com>
2026-04-15 19:16:49 +0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
@@ -8,7 +9,8 @@ def main():
     return "OK"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     server = Thread(target=run)
